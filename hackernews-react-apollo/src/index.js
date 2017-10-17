6c5d023bb3ba70807daf6898ee6a376ceb6e4676
Oleksandr Sachuk <oleksandr.sachuk@syndicode.co>
2017-10-17 10:14:34 +0300

    Realtime Updates with GraphQL Subscriptions

--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -6,9 +6,24 @@ import registerServiceWorker from './registerServiceWorker';
 import {ApolloProvider, createNetworkInterface, ApolloClient} from 'react-apollo'
 import { BrowserRouter } from 'react-router-dom'
 import { GC_AUTH_TOKEN } from './components/constants'
+import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws'
+
 const networkInterface = createNetworkInterface({
   uri: 'https://api.graph.cool/simple/v1/cj8l7nzk301v30140990tr8sd\n'
 })
+
+const wsClient = new SubscriptionClient('wss://subscriptions.graph.cool/v1/cj8l7nzk301v30140990tr8sd', {
+  reconnect: true,
+  connectionParams: {
+    authToken: localStorage.getItem(GC_AUTH_TOKEN),
+  }
+})
+
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+  networkInterface,
+  wsClient
+)
+
 networkInterface.use([{
   applyMiddleware(req, next) {
     if (!req.options.headers) {
@@ -21,8 +36,9 @@ networkInterface.use([{
 }])
 
 const client = new ApolloClient({
-  networkInterface
+  networkInterface: networkInterfaceWithSubscriptions
 })
+
 ReactDOM.render(
   <BrowserRouter>
     <ApolloProvider client={client}>
